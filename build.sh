704e0329b290c1627898883e53e84fe2e297ec54
Marjorie H <hirschim@bu.edu>
2026-08-26 16:32:00 -0400

    Fix migration command in build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
+
 pip install -r requirements.txt
+
 python Pizza_bidding_app/manage.py collectstatic --noinput
 python Pizza_bidding_app/manage.py migrate
-et -o errexit
